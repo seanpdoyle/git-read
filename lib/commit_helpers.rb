65c22c45a952256bf9a8b8f949db9e2cc73555e4
Sean Doyle <sean.p.doyle24@gmail.com>
2020-02-09 00:38:47 -0500

    introduce CommitHelpers
    Declare [Custom Helpers]
    
    [Custom Helpers]: https://middlemanapp.com/basics/helper-methods/#custom-defined-helpers

---
+++ lib/commit_helpers.rb
@@ -0,0 +1,16 @@
+module CommitHelpers
+  def commit?
+    current_page.data.id.present?
+  end
+
+  def current?(commit)
+    [
+      current_page.data.id,
+      commit.id
+    ].map(&:to_s).uniq.size == 1
+  end
+
+  def commit_path(commit)
+    "/commits/#{commit.id}"
+  end
+end
