58d4d4f04fe5a4d09ddcbc6fc69d096b9cf15bdd
Sean Doyle <sean.p.doyle24@gmail.com>
2020-03-16 02:28:07 -0400

    Dynamically determine project title
    Prior to this commit, the application's page title was hard-coded to be
    `git-read`.
    
    For consumer projects, that title won't apply. In order to determine the
    value to render as the title, invoke
    [`Pathname#basename`][Pathname#basename], which delegates to
    [`File.basename`][File.basename] to determine the name of the working
    directory that the Git repository exists in.
    
    Once that value is determined, make it available as part of the
    `history` Hash declared as a template-local variable.
    
    To ensure that an operating system's path quirks don't result in invalid
    HTML, wrap the project name with a call to [`escape_html`][escape_html].
    
    [Pathname#basename]: https://ruby-doc.org/stdlib-2.7.0/libdoc/pathname/rdoc/Pathname.html#method-i-basename
    [File.basename]: https://ruby-doc.org/core-2.7.0/File.html#method-c-basename
    [escape_html]: https://middlemanapp.com/basics/helper-methods/#format-helpers

--- test/system/visitor_views_readme_test.rb
+++ test/system/visitor_views_readme_test.rb
@@ -18,4 +18,19 @@ class VisitorViewsReadmeTest < ApplicationSystemTestCase
       assert_selector "p", text: "Hello, from tests!"
     end
   end
+
+  test "visitor navigates to README from another page" do
+    stage_file("README.md", "The README")
+    commit("Initial Commit")
+    stage_file("ignored.txt", "ignore me")
+    commit("Last commit")
+
+    with_git_repository do |directory|
+      visit "/index.html"
+      click_on "Last commit"
+      click_on directory.basename.to_s
+
+      assert_text "The README"
+    end
+  end
 end
