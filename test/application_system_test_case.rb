f27fe60d06cca57e080d0bdfff9dd02fcd3c0c73
Sean Doyle <sean.p.doyle24@gmail.com>
2020-03-16 01:13:32 -0400

    Extract `ApplicationSystemTestCase`
    In preparation for an additional [System Test][], this commit extracts
    the test harness' set-up and tear-down code into a super-class that the
    test case inherits from.
    
    To simplify `require` path resolution, and enable calling `require
    "application_system_test_case"` instead of `require_relative
    "./application_system_test_case"`, the test runner will need to append
    the `"test"` directory onto the `$LOAD_PATH`.
    
    To do so, declare a [`Rakefile`][Rakefile] that contains a
    [`Rake::TestTask`][test-task]. Passing `-W0` as "warning, level 0" is
    necessary, since our dependency on Middleman results in too much output
    noise without silencing Ruby warnings.
    
    To run the full test suite, execute:
    
    ```bash
    rake test
    ```
    
    [System Test]: https://guides.rubyonrails.org/testing.html#system-testing
    [Rakefile]: #Rakefile
    [test-task]: https://ruby.github.io/rake/Rake/TestTask.html

---
+++ test/application_system_test_case.rb
@@ -0,0 +1,49 @@
+require "bundler"
+Bundler.require(:default, :test)
+
+require "minitest/autorun"
+require "active_support"
+require "active_support/test_case"
+
+require "pathname"
+require "tmpdir"
+
+require "middleman/rack"
+
+class ApplicationSystemTestCase < ActiveSupport::TestCase
+  include Capybara::DSL
+  include Capybara::Minitest::Assertions
+
+  def around(&block)
+    directory = Dir.mktmpdir
+    begin
+      @repository = Git.init(directory)
+      block.call
+    ensure
+      FileUtils.rm_rf(directory)
+    end
+  end
+
+  def stage_file(filename, contents)
+    Pathname(@repository.dir.path).join(filename).write(contents)
+
+    @repository.add(filename)
+  end
+
+  def commit(message)
+    @repository.commit(message)
+  end
+
+  def with_git_repository(&block)
+    ClimateControl.modify(GIT_DIR: @repository.dir.path) do
+      Capybara.app = Middleman.server
+
+      block.call
+    end
+  end
+
+  teardown do
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
+end
