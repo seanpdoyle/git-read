1fbc5f4419ab05a5ffb18f9a7bf3b79ba43e9493
Sean Doyle <sean.p.doyle24@gmail.com>
2020-03-15 23:20:18 -0400

    Exercise the application through a System Test
    System Testing
    ---
    
    This commit adds a foundation for a test harness inspired by [Rails'
    System Tests][system-test].
    
    System tests are the [highest level][testing-pyramid] tests available to
    a web application, and serve as a proxy for the end-user experience.
    
    To drive the application, integrate with [Capybara's Rack mounting
    capabilities][capybara] by mounting [Middleman as a Rack
    application][middleman-rack].
    
    Dependencies
    ---
    
    Utilize [Bundler][bundler] to add a [`test` group][bundler-group]
    dependency on [Capybara][capybara] to visit the application from the
    test suite through its [domain specific language-style navigation
    helpers methods][capybara-dsl].
    
    Extend [Minitest's provided assertions][minitest] with
    [Capybara-specified assertions][capybara-minitest].
    
    It is important to [`require "minitest/autorun"`][minitest/autorun]
    explicitly, so that a Test Case executes all of the tests it declares,
    when executed from the comfort of our command line.
    
    To run the suite, execute:
    
    ```bash
    ruby -I test test/system/visitor_views_readme_test.rb
    ```
    
    [bundler]: https://bundler.io/v2.0/guides/using_bundler_in_applications.html
    [bundler-group]: https://bundler.io/v2.0/guides/groups.html
    [capybara]: https://github.com/teamcapybara/capybara/tree/3.31.0#setup
    [minitest]: https://github.com/seattlerb/minitest/tree/v5.14.0#label-Unit+tests
    [capybara-minitest]: https://github.com/teamcapybara/capybara/tree/3.31.0#using-capybara-with-minitest
    [capybara-dsl]: https://github.com/teamcapybara/capybara/tree/3.31.0#the-dsl
    [middleman-rack]: https://github.com/middleman/middleman/blob/v4.3.6/middleman-core/lib/middleman/rack.rb
    [testing-pyramid]: https://martinfowler.com/articles/practical-test-pyramid.html#TheTestPyramid
    [system-test]: https://guides.rubyonrails.org/testing.html#system-testing
    [minitest/autorun]: https://github.com/seattlerb/minitest/tree/v5.14.0#label-LICENSE-3A

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@ source 'https://rubygems.org'
 gem 'middleman', '~> 4.2'
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby, :x64_mingw]
 gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw, :x64_mingw]
+
+group :test do
+  gem 'capybara', require: 'capybara/minitest'
+end
---
+++ test/system/visitor_views_readme_test.rb
@@ -0,0 +1,28 @@
+require "bundler"
+
+Bundler.require(:default, :test)
+
+ENV["MODE"] ||= "build"
+
+require "minitest/autorun"
+require "middleman/rack"
+require "active_support"
+require "active_support/test_case"
+
+Capybara.app = Middleman.server
+
+class VisitorViewsReadmeTest < ActiveSupport::TestCase
+  include Capybara::DSL
+  include Capybara::Minitest::Assertions
+
+  teardown do
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
+
+  test "visitor views README" do
+    visit "/index.html"
+
+    assert_text "git-read"
+  end
+end
