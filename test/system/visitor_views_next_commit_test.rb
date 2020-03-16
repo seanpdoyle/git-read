6d005bbdcf7b5f6040948dcf975c3c3329ef687b
Sean Doyle <sean.p.doyle24@gmail.com>
2020-03-16 02:13:33 -0400

    Navigate to next commit
    When one commit comes after another in a sequence, it can be referred
    to as its child.
    
    Unfortunately, the concept of a child in a directed graph like a Git
    tree (especially a reversed Git tree, like in our case) does not apply.
    
    To counteract this limitation, introduce a `adjacent_commits` helper
    method to iterate through the history with a three commit-wide window,
    determining whether or not the page's current commit' SHA matches the
    `middle` element of the window's SHA.
    
    In order to add a buffer to either side of the "window", this approach
    utilizes the [`Enumerable#chain`][chain] method to combine the Array of
    Git commits with extra leading and trailing indices, to give the
    windowing function space to execute.
    
    When viewing the final commit, the commit won't have a child.
    
    To account for this, guard the render block with a [presence checking
    conditional][present?].
    
    Desktop: Only Next
    ---
    
    ![git-read navigation with only "Next" in desktop dimensions][desktop-only-next]
    
    Desktop: Only Previous
    ---
    
    ![git-read navigation with only "Previous" in desktop dimensions][desktop-only-previous]
    
    Mobile: Only Next
    ---
    
    ![git-read navigation with only "Next" in mobile dimensions][mobile-only-next]
    
    Mobile: Only Previous
    ---
    
    ![git-read navigation with only "Previous" in mobile dimensions][mobile-only-previous]
    
    [present?]: https://api.rubyonrails.org/classes/Object.html#method-i-present-3F
    [chain]: https://ruby-doc.org/core-2.7.0/Enumerable.html#method-i-chain
    [desktop-only-next]: https://user-images.githubusercontent.com/2575027/78401388-a3c82a00-75c6-11ea-8c41-5a421f16df55.png
    [desktop-only-previous]: https://user-images.githubusercontent.com/2575027/78401389-a460c080-75c6-11ea-8392-c5635f87a2f4.png
    [mobile-only-next]: https://user-images.githubusercontent.com/2575027/78401390-a460c080-75c6-11ea-801e-8f2d4fa1a3cf.png
    [mobile-only-previous]: https://user-images.githubusercontent.com/2575027/78401391-a460c080-75c6-11ea-9f76-9129ea4c988a.png

---
+++ test/system/visitor_views_next_commit_test.rb
@@ -0,0 +1,34 @@
+require "application_system_test_case"
+
+class VisitorViewsNextCommitTest < ApplicationSystemTestCase
+  test "visitor views next commit" do
+    stage_file("README.md", "First version")
+    commit("First commit")
+    stage_file("README.md", "Middle version")
+    commit("Middle commit")
+    stage_file("README.md", "Last version")
+    commit("Last commit")
+
+    with_git_repository do
+      visit "/index.html"
+      expand_history
+      click_on "First commit"
+      click_on "Next"
+
+      assert_selector "h1", text: "Middle commit"
+    end
+  end
+
+  test "visitor views commit without a next commit" do
+    stage_file("README.md", "First version")
+    commit("First commit")
+
+    with_git_repository do
+      visit "/index.html"
+      expand_history
+      click_on "First commit"
+
+      assert_no_link "Next"
+    end
+  end
+end
