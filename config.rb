9469b52bbdf746fdd6a8da332425202c03aa3e49
Sean Doyle <sean.p.doyle24@gmail.com>
2020-03-16 10:44:41 -0400

    Add "current" styles to `<a>` elements
    When browsing the history, the current page (as determined by the
    [`middleman-aria_current`][middleman-aria_current] extension, will
    annotate the `<a>` element with the [`[aria-current="page"]`
    attribute][aria-current].
    
    According to the `middleman-aria_current` documentation, the
    `:not([aria-current="false"])` portion of the CSS declaration is
    significant and intentional:
    
    > Note that we _exclude_ styling the link if `aria-current` has a value
    > of `false`. This is because `false` is a valid and useful value for
    > denoting a link that does not represent the current item within a set.
    
    When a commit is "current", its indicator is colored. Otherwise, they
    render as the typography color:
    
    ![highlighted commit indicator](https://user-images.githubusercontent.com/2575027/76769820-500ab380-6773-11ea-8290-a20d4f41d636.png)
    
    [middleman-aria_current]: https://github.com/thoughtbot/middleman-aria_current
    [aria-current]: https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques

--- config.rb
+++ config.rb
@@ -1,6 +1,7 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
+activate :aria_current
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
