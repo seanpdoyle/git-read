fbda59dc19071119e8602e8c7bf01ba8117c48d3
Sean Doyle <sean.p.doyle24@gmail.com>
2020-03-17 08:13:59 -0400

    Extract `GitHelpers` from System Test Cases
    The majority of the test helpers declared in the
    [`ApplicationSystemTestCase`](#test/application_system_test_case.rb) are
    focused on `git` operations.
    
    In an effort to prevent the
    [`ApplicationSystemTestCase`](#test/application_system_test_case.rb)
    from growing unchecked, this commit extracts the
    [`GitHelpers`](#test/git_helpers.rb), then calls
    [`Module.include`][include] in to mix the helpers back into the parent
    class.
    
    [include]: https://ruby-doc.org/core-2.7.0/Module.html#method-i-include

---
+++ test/git_helpers.rb
@@ -0,0 +1,54 @@
+require "middleman/rack"
+require "pathname"
+require "tmpdir"
+
+module GitHelpers
+  def stage_file(filename, contents)
+    Pathname(@repository.dir.path).join(filename).write(contents)
+
+    @repository.add(filename)
+  end
+
+  def commit(message)
+    @repository.commit(message)
+  end
+
+  def with_built_output(&block)
+    git_directory = Pathname(@repository.dir.path)
+    build_directory = git_directory.join("build")
+
+    system <<~BASH, exception: true
+    bin/git-read \
+      --verbose \
+      --git-dir #{git_directory} \
+      --output-dir #{build_directory}
+    BASH
+
+    Capybara.app = Rack::Builder.new do
+      use Rack::Static,
+        urls: [""],
+        root: build_directory,
+        index: "index.html"
+
+      run -> (*) {}
+    end
+
+    block.call
+  end
+
+  def with_git_repository(&block)
+    ClimateControl.modify(GIT_DIR: @repository.dir.path) do
+      Capybara.app = Middleman.server
+
+      block.call Pathname(@repository.dir.path)
+    end
+  end
+
+  def with_git_directory(&block)
+    directory = Dir.mktmpdir
+    @repository = Git.init(directory)
+    block.call
+  ensure
+    FileUtils.rm_rf(directory)
+  end
+end
