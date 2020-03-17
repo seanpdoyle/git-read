cd81aed41b5b4d59686a14498b8eecfd37153501
Sean Doyle <sean.p.doyle24@gmail.com>
2020-03-17 08:13:59 -0400

    Extract `GitHelpers` from System Test Cases
    The majority of the test helpers declared in the
    [`ApplicationSystemTestCase`][test/application_system_test_case.rb] are
    focused on `git` operations.
    
    In an effort to prevent the
    [`ApplicationSystemTestCase`][test/application_system_test_case.rb] from
    growing unchecked, this commit extracts the
    [`GitHelpers`][test/git_helpers.rb], then calls
    [`Module.include`][include] in to mix the helpers back into the parent
    class.
    
    [test/application_system_test_case.rb]: #test/application_system_test_case.rb
    [test/git_helpers.rb]: #test/git_helpers.rb
    [include]: https://ruby-doc.org/core-2.7.0/Module.html#method-i-include

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,70 +1,20 @@
 require "test_helper"
 
-require "pathname"
-require "tmpdir"
-
-require "middleman/rack"
-
 require_relative "../helpers/url_helpers"
+require "git_helpers"
 
 class ApplicationSystemTestCase < ActiveSupport::TestCase
   include Capybara::DSL
   include Capybara::Minitest::Assertions
   include UrlHelpers
+  include GitHelpers
 
   def around(&block)
-    directory = Dir.mktmpdir
-    begin
-      @repository = Git.init(directory)
-      block.call
-    ensure
-      FileUtils.rm_rf(directory)
-    end
+    with_git_directory(&block)
   end
 
   def expand_history
     find("summary", text: "Expand History").click
-  end
-
-  def stage_file(filename, contents)
-    Pathname(@repository.dir.path).join(filename).write(contents)
-
-    @repository.add(filename)
-  end
-
-  def commit(message)
-    @repository.commit(message)
-  end
-
-  def with_built_output(&block)
-    git_directory = Pathname(@repository.dir.path)
-    build_directory = git_directory.join("build")
-
-    system <<~BASH, exception: true
-      bin/git-read \
-        --verbose \
-        --git-dir #{git_directory} \
-        --output-dir #{build_directory}
-    BASH
-
-    Capybara.app = Rack::Builder.new do
-      use Rack::Static,
-        urls: [""],
-        root: build_directory,
-        index: "index.html"
-
-      run -> (*) {}
-    end
-
-    block.call
-  end
-
-  def with_git_repository(&block)
-    ClimateControl.modify(GIT_DIR: @repository.dir.path) do
-      Capybara.app = Middleman.server
-
-      block.call Pathname(@repository.dir.path)
-    end
   end
 
   teardown do
