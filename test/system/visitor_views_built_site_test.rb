54dda3db29b3cea6d0534fe7d959c4b61617eb5d
Sean Doyle <sean.p.doyle24@gmail.com>
2020-03-16 23:28:26 -0400

    Introduce `bin/git-read` command line interface
    This commit introduces the [`bin/git-read`][bin/git-read] executable. So
    far, the executable can be passed three flags.
    
    To view the possible options, execute with the `--help` flag to output
    its usage documentation:
    
    ```
    usage: git read [--help] [-v|--verbose] [-g|--git-dir <path>] [-o|--output-dir <path>]
    
    Generate HTML from your project's Git history
    
    OPTIONS
          --output-dir
              specify the directory where the site will be built
    
          --git-dir
              specify the parent directory of the project's .git/ directory
    ```
    
    The end result of a successful [`bin/get-read`][bin/git-read] execution
    is that [`middleman build`][middleman-build] output will be written to
    the directory passed as the `--output-dir` flag.
    
    Test coverage
    ---
    
    To ensure that the output of the [`bin/git-read`][bin/git-read] command
    is the same as what's covered in other System Test cases declared in
    `test/system`, extract the `#with_built_output` helper.
    
    The `#with_built_output` leverages the test harness' test-isolated
    temporary directory, and invokes `bin/git-read` to read from and write
    to a `build/` directory within the tmpdir. The contents of the `build/`
    directory are served by a constructed through a [`Rack::Static`
    middleware][rack-static] [`Rack::Builder` domain specific
    language][rack-builder].
    
    To ensure that executions of [`bin/git-read`][bin/git-read] exit with a
    `0` status code, this commit introduces the first test case outside of
    the `test/system` directory.
    
    Since that test case doesn't require all of the System Test case setup,
    the [`test/test_helper.rb`][test/test_helper.rb] file serves as a
    re-usable chunk, shared by both styles of test.
    
    [bin/git-read]: #bin/git-read
    [test/test_helper.rb]: #test/test_helper.rb
    [middleman-build]: https://middlemanapp.com/basics/build-and-deploy#deploying-the-site
    [rack-static]: https://www.rubydoc.info/github/rack/rack/master/Rack/Static
    [rack-builder]: https://www.rubydoc.info/github/rack/rack/master/Rack/Builder

---
+++ test/system/visitor_views_built_site_test.rb
@@ -0,0 +1,15 @@
+require "application_system_test_case"
+
+class VisitorViewsBuiltSiteTest < ApplicationSystemTestCase
+  test "visitor views built site" do
+    stage_file("README.md", "Ignore me")
+    commit("From the command line")
+
+    with_built_output do
+      visit root_path
+      click_on "From the command line"
+
+      assert_selector "h1", text: "From the command line"
+    end
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+require "bundler"
+Bundler.require(:default, :test)
+
+require "minitest/autorun"
+require "active_support"
+require "active_support/test_case"
