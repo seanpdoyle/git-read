af221228154bfd13a2717d46b4e1122341eeb35a
Sean Doyle <sean.p.doyle24@gmail.com>
2020-03-17 08:14:30 -0400

    Add support for re-writing a project's history
    If you're consuming this tool and using it to tell the story of your
    project's origins and growth, you might be tempted to become a bit of a
    revisionist historian.
    
    This might come as a shock, but this project was not perfected
    implemented from the start, and is in fact the result of countless
    [`pick`, `squash`, `reword`, `fixup`, and `drop` rebase
    operations][rebase].
    
    While this can be harmless for pre-released projects, rewriting history
    once the resulting website is deployed and publicized would result in
    [`404`s and broken URLs][404].
    
    To counteract this, `git-read` will include and compile pages for any
    commits that are [`git tag`-ed][git-tag], along with the rest of their
    history.
    
    To denote that a commit is "out of date", and is no longer available on
    `master`, calls to Middleman's [`proxy`][proxy] have been updated to
    pass in the `out_of_date` local Boolean to denote whether or not a page
    is built from a tagged commit. When rendering from `master` or the
    project's `README.md`, `out_of_date` is declared as `false`.
    
    In the case where `master`, and the tagged commit share ancestors, the
    `master` version will be rendered second, and won't display the "stale"
    banner.
    
    Test coverage
    ---
    
    To ensure that the new feature is covered by tests, this commit extends
    the new [`GitHelpers` module][test/git_helpers.rb] to enable tests to
    switch `git` branches (via `#switch_branch`), and to add tagged commits
    (via `#add_tag`).
    
    [rebase]: https://git-scm.com/book/en/v2/Git-Tools-Rewriting-History#_changing_multiple
    [404]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/404
    [git-tag]: https://git-scm.com/book/en/v2/Git-Basics-Tagging
    [proxy]: https://middlemanapp.com/advanced/dynamic-pages#defining-proxies
    [test/git_helpers.rb]: #test/git_helpers.rb

---
+++ test/system/visitor_views_a_stale_commit_test.rb
@@ -0,0 +1,43 @@
+require "application_system_test_case"
+
+class VisitorViewsAStaleCommitTest < ApplicationSystemTestCase
+  test "visitor views a stale commit" do
+    stage_file("README.md", "Ignore me ")
+    commit("Initial commit")
+    branch_commit_sha = switch_branch("my-branch") do
+      stage_file("file.txt", "Ignore me")
+      commit("Hello from the branch")
+      tag_head_commit("my-tag")
+    end
+
+    with_git_repository do
+      visit commit_path(branch_commit_sha)
+      expand_history
+
+      assert_text "You're viewing an out-of-date version of the project."
+      assert_selector "a", text: "Read the latest version."
+      assert_selector "h1", text: "Hello from the branch"
+      assert_selector "[aria-current]", text: "Hello from the branch"
+    end
+  end
+
+  test "visitor views a does not see stale commit from master" do
+    stage_file("README.md", "Ignore me")
+    commit("Hello from master")
+    switch_branch("my-branch") do
+      stage_file("file.txt", "Ignore me")
+      commit("Hello from the branch")
+      tag_head_commit("my-tag")
+    end
+
+    with_git_repository do
+      visit root_path
+      expand_history
+
+      assert_selector "a", text: "Hello from master"
+      assert_no_text "You're viewing an out-of-date version of the project."
+      assert_no_selector "a", text: "Read the latest version."
+      assert_no_selector "a", text: "Hello from the branch"
+    end
+  end
+end
