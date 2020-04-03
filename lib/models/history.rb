36b990f020e797429c12b547d45bc59903227cf3
Sean Doyle <sean.p.doyle24@gmail.com>
2020-04-02 22:22:05 -0400

    Render history of tag, commits to home link
    BUGFIX: Render "Next" from root

---
+++ lib/models/history.rb
@@ -0,0 +1,27 @@
+class History
+  attr_reader :readme, :repository, :root
+
+  def initialize(readme:, repository:, root:, out_of_date: false)
+    @readme = readme
+    @root = root
+    @repository = repository
+    @out_of_date = out_of_date
+  end
+
+  def out_of_date?
+    @out_of_date
+  end
+
+  def name
+    File.basename(repository.dir.path)
+  end
+
+  def commits
+    root.log(nil).reverse_each.map do |commit|
+      Commit.new(
+        commit: commit,
+        repository: repository,
+      )
+    end
+  end
+end
