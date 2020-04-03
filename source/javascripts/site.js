7d581e63f3ecda110fe1214dee40f16507eec84e
Sean Doyle <sean.p.doyle24@gmail.com>
2020-04-03 00:25:35 -0400

    Scroll to current commit during navigation
    When the page loads, a [`turbolinks:load`][turbolinks:load] event fires.
    
    When that event fires, find the commit in the history that has
    [`[aria-current="page"]`][aria-current] and scroll its viewport
    (vertically aligned on desktop, horizontally aligned on smaller devices)
    until the element is centered by invoking [`.scrollIntoView()` with
    `block:` and `inline:` options][scrollIntoView].
    
    [turbolinks:load]: https://github.com/turbolinks/turbolinks/tree/v5.2.0#full-list-of-events
    [aria-current]: https://tink.uk/using-the-aria-current-attribute/
    [scrollIntoView]: https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView#Parameters

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,3 +1,9 @@
 import Turbolinks from "turbolinks"
 
 Turbolinks.start()
+
+document.addEventListener("turbolinks:load", () => {
+  for (const current of document.querySelectorAll('[aria-current="page"]')) {
+    current.scrollIntoView({ block: "center", inline: "center" })
+  }
+})
