b678bf5720267b79d3038ded381d039f24038958
Sean Doyle <sean.p.doyle24@gmail.com>
2020-04-03 00:51:16 -0400

    order with grid and flex order

--- source/javascripts/packs/application.js
+++ source/javascripts/packs/application.js
@@ -4,6 +4,6 @@ Turbolinks.start()
 
 document.addEventListener("turbolinks:load", () => {
   for (const current of document.querySelectorAll('[aria-current="page"]')) {
-    current.scrollIntoView({ block: "center", inline: "center" })
+    current.scrollIntoView({ inline: "center" })
   }
 })
